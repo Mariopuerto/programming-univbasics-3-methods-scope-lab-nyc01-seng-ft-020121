1d99146c6fe72b6150e93002161b5b1ebbf7cfea
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-20 19:38:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -3,7 +3,6 @@ describe "mario" do
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
   end
 
-
 describe "toadstool" do
   it "puts out 'Thank You Mario! But Our Princess Is In Another Castle!'" do
     expect{toadstool}.to output("Thank You Mario! But Our Princess Is In Another Castle!\n").to_stdout
