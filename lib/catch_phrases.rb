31bb0591feaf1e23b0f106c93b8d3b6f0d91d55b
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-20 20:41:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,16 +1,17 @@
-def mario
-  puts
+def mario = ("It's-a me, Mario!\n")
+  puts ("It's-a me, Mario!\n") 
+  mario
 end
 
 
 def toadstool
-  puts
+  puts 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
 
 def link
-  puts
+  puts 'Thank You Mario! But Our Princess Is In Another Castle!'
 end
 
 def all_phrases
-  puts
+  puts 
 end
